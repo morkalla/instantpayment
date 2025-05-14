1b6007f3418c169742eae5a46ce1e08d3f2ccd30
morkalla <kallamor92@gmail.com>
2025-05-14 12:16:13 +0200

    add recover option

--- src/main/java/hu/morkalla/instantpayment/service/DefaultPaymentService.java
+++ src/main/java/hu/morkalla/instantpayment/service/DefaultPaymentService.java
@@ -2,12 +2,15 @@ package hu.morkalla.instantpayment.service;
 
 import hu.morkalla.instantpayment.domain.Account;
 import hu.morkalla.instantpayment.domain.Transaction;
+import hu.morkalla.instantpayment.exception.EntityNotFoundException;
 import hu.morkalla.instantpayment.exception.NotEnoughBalance;
 import hu.morkalla.instantpayment.exception.TransactionAlreadyProcessed;
 import hu.morkalla.instantpayment.rest.domain.PaymentRequestDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.StaleObjectStateException;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +25,9 @@ public class DefaultPaymentService implements PaymentService {
 
     @Override
     @Transactional
-    @Retryable(retryFor = StaleObjectStateException.class)
+    @Retryable(retryFor = StaleObjectStateException.class,
+            notRecoverable = {NotEnoughBalance.class, TransactionAlreadyProcessed.class, EntityNotFoundException.class},
+            backoff = @Backoff(delay = 100))
     public void transfer(PaymentRequestDto paymentRequestDto) {
 
         final String transactionId = paymentRequestDto.transactionId();
@@ -36,7 +41,7 @@ public class DefaultPaymentService implements PaymentService {
 
         BigDecimal transactionAmount = paymentRequestDto.transactionAmount();
         BigDecimal newSourceBalance = sourceAccount.getBalance().subtract(transactionAmount);
-        
+
         if (isSourceBalanceValid(newSourceBalance)) {
             sourceAccount.setBalance(newSourceBalance);
             targetAccount.setBalance(targetAccount.getBalance().add(transactionAmount));
@@ -49,6 +54,20 @@ public class DefaultPaymentService implements PaymentService {
         } else {
             throw new NotEnoughBalance("Not enough balance for transaction: " + transactionId);
         }
+    }
+
+    @Recover
+    public void recover(StaleObjectStateException exception, PaymentRequestDto paymentRequestDto) {
+        Account sourceAccount =
+                accountService.findAccountByAccountNumber(paymentRequestDto.sourceAccountNumber());
+        Account targetAccount =
+                accountService.findAccountByAccountNumber(paymentRequestDto.targetAccountNumber());
+
+        transactionService.saveTransaction(
+                new Transaction(null, paymentRequestDto.transactionId(),
+                        sourceAccount.getId(),
+                        targetAccount.getId(),
+                        paymentRequestDto.transactionAmount(), paymentRequestDto.transactionDate(), "FAILED"));
 
     }
 
